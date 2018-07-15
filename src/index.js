20f83ea6990b748f707b43e92a3c872fbe570ea0
Colin Thompson <thompsoncolinm@gmail.com>
2018-07-15 16:28:33 -0400

    working files

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 ReactDOM.render(
-<div>
-<h1>Hello There</h1>
-<p>This is an element for me to insert into my app</p>
-
-</div>
-
-    , document.getElementById('root'))
+  <div>
+    <h1>Hello world!</h1>
+  </div>,
+  document.getElementById('root')
+)
