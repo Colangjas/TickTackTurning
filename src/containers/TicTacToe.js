d893725c5e1675539f008b233365babb6201e8bf
Colin Thompson <thompsoncolinm@gmail.com>
2018-08-28 06:51:13 -0400

    fished chapter

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -1,11 +1,53 @@
 import React, {Component} from 'react'
+import {Stage} from 'react-konva'
 
 class TicTacToe extends Component {
+
+  state = {
+
+  }
+  
+  componentWillMount() {
+    let height = window.innerHeight
+    let width = window.innerWidth
+    let size = (height < width) ? height * .8 : width * .8
+    let rows = this.state.rows
+    let unit = size / rows
+
+    this.setState({
+      size,
+      rows,
+      unit
+    })
+
+  }
+
+  move = () => {
+    //pleaceholder  
+  }
+
+  makeAiMove = () => {
+    //pleaceholder
+  }
+
+  turningTest = () => {
+    //pleaceholder
+  }
+
+  recordGame = () => {
+    //pleaceholder
+  }
 
   render() {
     return (
       <div>
-        <h2>Home</h2>
+        <Stage
+        width={}
+        height={}
+        >
+          {/* <Board /> */}
+          {/* <Squares /> */}
+        </Stage>
       </div>
     )
   }
